9e4500883589e46edcff032360f57b519420a870
kanvara-2547 <lanna71047@gmail.com>
2023-12-29 21:27:36 +0700

    complete homework

--- main.py
+++ main.py
@@ -3,12 +3,21 @@
 """
 
 ### str
-
+name = "Lanna"
+animal = "monkey"
+animal_1 = "cat"
 
 ### int
-
-
+n = 1
+v = 2
+c = 5
 ### float
-
-
+p = 2.5
+k = 3.0
+i = 5.6
 ### bool
+is_eat = True
+is_die = False
+is_king = True
+
+print(name, animal_1, animal, n, v, c, p, k, i, is_die, is_eat, is_king)
